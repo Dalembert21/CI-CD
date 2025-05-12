9cbe9dc7c22ec081fc470d934c81ddbecf494abc
Dalembert31 <dalembertbravo2@gmail.com>
2025-05-12 11:01:30 -0500

    creacion de metodos para saludar y probar la automatizacion

---
+++ hola.py
@@ -0,0 +1,4 @@
+from practica import saludar
+
+def test_saludo():
+    assert saludar() == "Hola esta es mi primera practica de CI/CD"
--- practica.py
+++ practica.py
@@ -1,1 +1,6 @@
 #practica de automtizacion 
+def saludar():
+    return "Hola esta es mi primera practica de CI/CD"
+
+if __name__ == "__main__":
+    print(saludar())
